4c6ec316a7ef72a889cf4e204bfc88ed8cd87722
Rohitkumar-tech <rohitkumar620200@gmail.com>
2024-03-25 15:25:45 +0530

    Updated the style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import Markdownreact from "./markdown/markdownreact";
 
 function App() {
--- src/markdown/markdownreact.js
+++ src/markdown/markdownreact.js
@@ -3,12 +3,17 @@ import Markdown from 'markdown-to-jsx';
 import './style.css';
 
 const MarkdownReact = () => {
-    const [markdown, setMarkdown] = useState("Welcome to Markdown");
+    const [markdown, setMarkdown] = useState("");
   
     return (
         <div className='center-div'>
-            <textarea className="left-side" value={markdown} onChange={(e) => setMarkdown(e.target.value)}></textarea>
-            <div className="right-side">
+            <div className='left-side'>
+                <p>Edit</p>
+            <textarea className="left-side" value={markdown} onChange={(e) => setMarkdown(e.target.value)} rows="15" cols="28">
+            </textarea>
+            </div>
+            <div className="right-side" >
+                <text>Preview</text>
                 <Markdown>{markdown}</Markdown>
             </div>
         </div>
